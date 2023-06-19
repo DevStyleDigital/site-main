01d288dc5d5f8168056c4311d5aaa705e3225d1e
Afonso Bezerra <afonsobezerra199@gmail.com>
2023-06-19 19:22:23 -0300

    add GTM TAG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import 'swiper/css'
 import 'swiper/css/navigation'
 import './global.css'
 import { SideBarRoot } from '@/components/app/sidebar'
+import Script from 'next/script';
 
 const nunito = Nunito({
   weight: ['400', '500', '600', '700', '800'],
@@ -16,12 +17,32 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
       <link rel="icon" href="/icon.png" sizes="any" />
-      </head>
+      {/* Google Tag Manager */}
+      <Script id="google-analytics" strategy="afterInteractive">
+      {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MB546VV');
+      `}
+      </Script>
+      {/* End Google Tag Manager */}
+      </head>      
       <body className={nunito.className}>
         <SideBarRoot>
           <Header />
           {children}
         </SideBarRoot>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MB546VV"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                `,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
       </body>
     </html>
   )
